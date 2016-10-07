9a8c0ab51e61fa862f775d673d9fd01b4a842990
Oezen <Oezen.Beyazsahin@ids.de>
2016-10-07 11:58:55 +0200

    Rechner

---
+++ src/main/java/de/ids/Clean/Rechner.java
@@ -0,0 +1,16 @@
+package de.ids.Clean;
+
+public class Rechner {
+	int a;
+	int o;
+
+	public int add(int a, int o) {
+
+		int c = a + o;
+		System.out.println("Hallo du schoenes Programm");
+		System.out.println("Hallo du NICHT schoenes Programm");
+
+		return c;
+	}
+
+}
